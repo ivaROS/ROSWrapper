d6e6056ca21f9fb49083b67be9acc9717b6adbd5
Yiye Chen <2019cyy@gmail.com>
2021-07-15 00:17:57 -0400

    reorganize the repo

---
+++ subscribers/Images_sub.py
@@ -0,0 +1,28 @@
+# =================================== Images_sub.py ==================================
+"""
+    @brief:     Subscribe image topics from the ROS, turn into numpy, 
+                and then trigger the callback
+        
+    @Author: Yiye Chen              yychen2019@gatech.edu
+    @date: 07/13/2021
+"""
+# =================================== Images_sub.py ==================================
+from Base import Base
+
+class Images_sub(Base):
+    def __init__(self, image_topic_list, messageType_list=None, callback_np=None):
+        """
+        @param[in] callback_np     should be a function on numpy image array
+        """
+        super().__init__(image_topic_list, messageType_list, 
+                        lambda images: self.process(images, callback_np))
+
+    def process(self, images_ROS, callback_np):
+        image_np = self._preprocess(images_ROS)
+        callback_np(image_np)
+
+    def _preprocess(self, images_ROS):
+        image_np = images_ROS
+        return image_np
+
+    
