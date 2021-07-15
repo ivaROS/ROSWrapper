b6d7007967448d9946379be0416c3fd093a38d8b
Yiye Chen <2019cyy@gmail.com>
2021-07-15 16:37:47 -0400

    Test scripts for the publishers

---
+++ tests/test_publishers_images.py
@@ -0,0 +1,64 @@
+# ========================================= test_publishers_images.py ================================
+"""
+
+    @brief          Test the Image publisher class: publishers.Image_pub.Image_pub
+
+    @Author:        Yiye Chen       yychen2019@gatech.edu
+    @date:          07/15/2021
+
+"""
+# ========================================= test_publishers_images.py ================================
+
+
+import os
+import sys
+import cv2
+import numpy as np
+import time
+
+# setup the environment - add the root path
+fpath = os.path.dirname(__file__)
+rpath = os.path.dirname(fpath)
+dpath = os.path.join(fpath, 'data')
+sys.path.append(rpath)
+
+from publishers.Image_pub import Image_pub
+
+def read_npz(filename):
+    """
+    for the npz file saved with python2,
+    python 3 and python 2 read the file differently.
+
+    see: https://stackoverflow.com/a/65414582/5269146
+    """
+    if(sys.version_info[:3] <3.0):
+        data = np.load(filename)
+    else:
+        data = np.load(filename, 
+            allow_pickle=True,fix_imports=True,encoding='latin1')
+    return data
+
+
+if __name__ == "__main__":
+    # prepare data
+    img = cv2.imread(os.path.join(dpath, "empty_desk_0.png"))
+    depth = read_npz(os.path.join(dpath, "empty_desk_data_0.npz"))["depth_frame"]
+
+    # prepare publishers
+    rgb_pub = Image_pub("Test_pub_image_rgb")
+    dep_pub = Image_pub("Test_pub_image_dep")
+
+    # publish
+    has_sent_notice = False
+    while(True):
+        rgb_pub.pub(img)
+        dep_pub.pub(dep_pub)
+
+        if not has_sent_notice:
+            print("\n\n Has published the rgb and the depth message to the topics: \n \
+                \"Test_pub_image_rgb\" and \"Test_pub_image_dep\" ") 
+            has_sent_notice = True
+
+        time.sleep(0.05)
+
+
---
+++ tests/test_publishers_mat.py
@@ -0,0 +1,63 @@
+# ========================================= test_publishers_images.py ================================
+"""
+
+    @brief          Test the Matrix publisher class: publishers.Matrix_pub.Matrix_pub
+
+    @Author:        Yiye Chen       yychen2019@gatech.edu
+    @date:          07/15/2021
+
+"""
+# ========================================= test_publishers_images.py ================================
+
+
+import os
+import sys
+import cv2
+import numpy as np
+import time
+
+# setup the environment - add the root path
+fpath = os.path.dirname(__file__)
+rpath = os.path.dirname(fpath)
+dpath = os.path.join(fpath, 'data')
+sys.path.append(rpath)
+
+from publishers.Matrix_pub import Matrix_pub
+
+def read_npz(filename):
+    """
+    for the npz file saved with python2,
+    python 3 and python 2 read the file differently.
+
+    see: https://stackoverflow.com/a/65414582/5269146
+    """
+    if(sys.version_info[:3] <3.0):
+        data = np.load(filename)
+    else:
+        data = np.load(filename, 
+            allow_pickle=True,fix_imports=True,encoding='latin1')
+    return data
+
+
+if __name__ == "__main__":
+    # prepare data
+    img = cv2.imread(os.path.join(dpath, "empty_desk_0.png"))
+    intrinsic_matrix = read_npz(os.path.join(dpath, "empty_desk_data_0.npz"))["intrinsics"]
+    print(intrinsic_matrix)
+
+    # prepare publishers
+    mat_pub = Matrix_pub("Test_pub_mat")
+
+    # publish
+    has_sent_notice = False
+    while(True):
+        mat_pub.pub(intrinsic_matrix)
+
+        if not has_sent_notice:
+            print("\n\n Has published the rgb and the depth message to the topics: \n \
+                \"Test_pub_mat\" ") 
+            has_sent_notice = True
+
+        time.sleep(0.05)
+
+
