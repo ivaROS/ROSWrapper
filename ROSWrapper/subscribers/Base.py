fd4c5bcf0e25249811170b8d7cc17a89f93cfe86
Yunzhi Lin <yunzhi.lin@gatech.edu>
2022-03-08 21:23:40 -0500

    add quene size

--- ROSWrapper/subscribers/Base.py
+++ ROSWrapper/subscribers/Base.py
@@ -19,14 +19,14 @@ class Base(object):
     def __init__(self, topic_list, messageType_list, callback):
         self.subs = [self.subscribe(topic, messageType) 
                 for (topic, messageType) in zip(topic_list, messageType_list)]
-        self.ts = ApproximateTimeSynchronizer(self.subs, 1, 0.1)
+        self.ts = ApproximateTimeSynchronizer(self.subs, 1, 1)
         self.ts.registerCallback(callback)
 
     def subscribe(self, topic, messageType):
         """
         Wait until an topic is published and then subscribe
         """
-        return Subscriber(topic, messageType)
+        return Subscriber(topic, messageType, queue_size=5)
 
     def receive(self):
         """
